cc57346467c291dcad3f0c951bf0d0f904a03c96
Raj <43986917+raj5152@users.noreply.github.com>
2019-12-02 18:13:56 +0530

    Add components (#8)
    * Add step, drawer, snackbar and idletimeout components
    
    * add step and other components

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,37 @@
+<template>
+   <v-snackbar 
+    class="white--text snackbar"
+    v-model="snackBar.enabled"
+    :timeout="snackBar.timeout"
+    :color="snackBar.color"
+    :top="snackBar.top"
+  >
+    {{ snackBar.message }}
+    <v-icon @click="snackBar.enabled = false" class="pa-4" dark>mdi-close</v-icon>
+  </v-snackbar>
+</template>
+<script> 
+
+export default {
+  name: 'DCO-Snackbar',
+  props: {
+      snackBar: {
+      type: Object,
+      default() {
+        return {
+          enabled:true,
+          timeout:40000,
+          color:'green',
+          top:true,
+          message:'Your message will display here !'
+        };
+      },
+    },
+  }
+};
+</script>
+<style>
+.v-snack__content {  
+  max-height: 50px;
+}
+</style>
